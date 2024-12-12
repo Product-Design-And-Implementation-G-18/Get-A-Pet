2d134b84c05fdad0167be2c8f30591630af58a94
Sujata Shrestha <shresthasujata054@gmail.com>
2024-12-12 18:30:13 +0200

    App polish

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,5 +1,7 @@
-export default {
-  PRIMARY: '#E8B20E',
-  GRAY: '#8F8e8d',
-  WHITE: '#ffffff',
-};
+export default{
+  PRIMARY:'#bf823a',
+  GRAY:'#8F8e96',
+  WHITE:'#fff',
+  SECONDARY:'#8a8827',
+  LIGHT_PRIMARY:'#dbb689'
+}
